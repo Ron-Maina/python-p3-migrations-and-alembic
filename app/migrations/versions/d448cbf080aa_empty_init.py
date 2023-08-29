6948b035c7ec9ca8bb5f4bd344fb45a6f383909e
Ron Maina <ronmwangi00@gmail.com>
2023-08-29 16:07:07 +0300

    Completed

---
+++ app/migrations/versions/d448cbf080aa_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: d448cbf080aa
+Revises: 
+Create Date: 2023-08-29 15:53:02.887170
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd448cbf080aa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
